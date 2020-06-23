01465fac35c033cc7010ceffdd0e03cae7e86cdf
Aman Sharma <amansharmalfs5@gmail.com>
2020-06-23 19:07:56 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,398 @@
+<?php
+include('db.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>HOTEL RIO</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Resort Inn Responsive , Smartphone Compatible web template , Samsung, LG, Sony Ericsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/font-awesome.css" rel="stylesheet">
+<link rel="stylesheet" href="css/chocolat.css" type="text/css" media="screen">
+<link href="css/easy-responsive-tabs.css" rel='stylesheet' type='text/css'/>
+<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" property="" />
+<link rel="stylesheet" href="css/jquery-ui.css" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<script type="text/javascript" src="js/modernizr-2.6.2.min.js"></script>
+<!--fonts-->
+<link href="//fonts.googleapis.com/css?family=Oswald:300,400,700" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Federo" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+<!--//fonts-->
+</head>
+<body>
+<!-- header -->
+	<div class="w3_navigation">
+		<div class="container">
+			<nav class="navbar navbar-default">
+				<div class="navbar-header navbar-left">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<h1><a class="navbar-brand" href="index.php">Hotel <span>Rio</span><p class="logo_w3l_agile_caption">Your Dreamy Resort</p></a></h1>
+				</div>
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+					<nav class="menu menu--iris">
+						<ul class="nav navbar-nav menu__list">
+							<li class="menu__item menu__item--current"><a href="" class="menu__link" f>Home</a></li>
+							<li class="menu__item"><a href="#about" class="menu__link scroll">About</a></li>
+							<li class="menu__item"><a href="#team" class="menu__link scroll">Team</a></li>
+							<li class="menu__item"><a href="#gallery" class="menu__link scroll">Gallery</a></li>
+							<li class="menu__item"><a href="#rooms" class="menu__link scroll">Rooms</a></li>
+							<li class="menu__item"><a href="#contact" class="menu__link scroll">Contact Us</a></li>
+						</ul>
+					</nav>
+				</div>
+			</nav>
+
+		</div>
+	</div>
+<!-- //header -->
+	<div id="home" class="w3ls-banner" style="background-image:url(images/2.jpg)">
+		<!-- banner-text -->
+                <div class="container">
+								<div class="agileits-banner-info">
+								<h4>HOTEL RIO</h4>
+									<h3>We know what you love</h3>
+										<p>Welcome to our hotels</p>
+
+		</div>
+	</div>
+</div>
+
+<!-- //Modal1 -->
+<div id="availability-agileits">
+<div class="col-md-12 book-form-left-w3layouts">
+	<a href="admin/reservation.php"><h2>ROOM RESERVATION</h2></a>
+</div>
+
+			<div class="clearfix"> </div>
+</div>
+<!-- banner-bottom -->
+	<div class="banner-bottom" where id = "about">
+		<div class="container">
+			<div class="agileits_banner_bottom">
+				<h3><span>Experience a good stay, enjoy fantastic offers</span> Find our friendly welcoming reception</h3>
+			</div>
+			<div class="w3ls_banner_bottom_grids">
+				<ul class="cbp-ig-grid">
+					<li>
+          		<span class="cbp-ig-icon w3_road"></span>
+							<h4 class="cbp-ig-title">MASTER BEDROOMS</h4>
+							</li>
+					<li>
+              <span class="cbp-ig-icon w3_cube"></span>
+							<h4 class="cbp-ig-title">SEA VIEW BALCONY</h4>
+							</li>
+					<li>
+							<span class="cbp-ig-icon w3_users"></span>
+							<h4 class="cbp-ig-title">LARGE CAFE</h4>
+							</li>
+					<li>
+             <span class="cbp-ig-icon w3_ticket"></span>
+							<h4 class="cbp-ig-title">WIFI COVERAGE</h4>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+<!-- //banner-bottom -->
+<!--sevices-->
+<div class="advantages">
+	<div class="container">
+		<div class="advantages-main">
+				<h3 class="title-w3-agileits">Our Services</h3>
+		   <div class="advantage-bottom">
+			 <div class="col-md-6 advantage-grid left-w3ls wow bounceInLeft" data-wow-delay="0.3s">
+			 	<div class="advantage-block ">
+
+			 		<h4>Stay First, Pay After! </h4>
+					<p><i class="fa fa-check" aria-hidden="true"></i>Decorated room, proper air conditioned</p>
+					<p><i class="fa fa-check" aria-hidden="true"></i>Private balcony</p>
+					<h4>24 Hour Restaurant</h4>
+					<p><i class="fa fa-check" aria-hidden="true"></i>24 hours room service</p>
+					<p><i class="fa fa-check" aria-hidden="true"></i>24-hour Concierge service</p>
+
+
+			 	</div>
+			 </div>
+			<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+</div>
+<!--//sevices-->
+<!-- team -->
+<div class="team" id="team">
+	<div class="container">
+			<h3 class="title-w3-agileits title-black-wthree">Meet Our Team</h3>
+			<div id="horizontalTab">
+					<ul class="resp-tabs-list">
+					<li>
+						<img src="images/aman.jpg" alt=" " class="img-responsive" />
+					</li>
+					<li>
+						<img src="images/vaibhav.jpg" alt=" " class="img-responsive" />
+					</li>
+					<li>
+						<img src="images/shantanu.jpg" alt=" " class="img-responsive" />
+					</li>
+					<li>
+						<img src="images/Aniruddha.jpg" alt=" " class="img-responsive" />
+					</li>
+					</ul>
+					<div class="resp-tabs-container">
+					<div class="tab1">
+						<div class="col-md-6 team-img-w3-agile">
+						</div>
+						<div class="col-md-6 team-Info-agileits">
+							<h4>Aman Sharma</h4>
+							<span>Owner</span>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<div class="tab2">
+					<div class="col-md-6 team-img-w3-agile">
+						</div>
+						<div class="col-md-6 team-Info-agileits">
+							<h4>Vaibhav Urkude</h4>
+							<span>Manager</span>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<div class="tab3">
+						<div class="col-md-6 team-img-w3-agile">
+						</div>
+						<div class="col-md-6 team-Info-agileits">
+							<h4>Shantanu Sharma</h4>
+							<span>Receptionist</span>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<div class="tab4">
+					<div class="col-md-6 team-img-w3-agile">
+						</div>
+						<div class="col-md-6 team-Info-agileits">
+							<h4>Aniruddha Yadav</h4>
+							<span>Chef</span>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					</div>
+			</div>
+	</div>
+</div>
+<!-- //team -->
+<!-- Gallery -->
+<section class="portfolio-w3ls" id="gallery">
+		 <h3 class="title-w3-agileits title-black-wthree">Our Gallery</h3>
+				<div class="col-md-3 gallery-grid gallery1">
+	      	<img src="images/g1.jpg" class="img-responsive" alt="/">
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+				 <img src="images/g2.jpg" class="img-responsive" alt="/">
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+      	 <img src="images/g3.jpg" class="img-responsive" alt="/">
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+			   <img src="images/g4.jpg" class="img-responsive" alt="/">
+			  </div>
+				<div class="col-md-3 gallery-grid gallery1">
+	       <img src="images/g5.jpg" class="img-responsive" alt="/">
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+					<img src="images/g6.jpg" class="img-responsive" alt="/">
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+				 <img src="images/g7.jpg" class="img-responsive" alt="/">
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+				 <img src="images/g8.jpg" class="img-responsive" alt="/">
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+        	<img src="images/g9.jpg" class="img-responsive" alt="/">
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+      	 <img src="images/g10.jpg" class="img-responsive" alt="/">
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+			  	<img src="images/g4.jpg" class="img-responsive" alt="/">
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+	       <img src="images/g2.jpg" class="img-responsive" alt="/">
+				</div>
+				<div class="clearfix"> </div>
+</section>
+<!-- //gallery -->
+	 <!-- rooms & rates -->
+      <div class="plans-section" id="rooms">
+				 <div class="container">
+				 <h3 class="title-w3-agileits title-black-wthree">Rooms And Rates</h3>
+						<div class="priceing-table-main">
+				 <div class="col-md-3 price-grid">
+					<div class="price-block agile">
+						<div class="price-gd-top">
+						<img src="images/r1.jpg" alt=" " class="img-responsive" />
+							<h4>Deluxe Room</h4>
+						</div>
+						<div class="price-gd-bottom">
+							<div class="price-selet">
+								<h3><span>Rs</span>20,000</h3>
+								<a href="admin/reservation.php" >Book Now</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 price-grid ">
+					<div class="price-block agile">
+						<div class="price-gd-top">
+						<img src="images/r2.jpg" alt=" " class="img-responsive" />
+							<h4>Luxury Room</h4>
+						</div>
+						<div class="price-gd-bottom">
+							<div class="price-selet">
+								<h3><span>Rs</span>15,000</h3>
+								<a href="admin/reservation.php" >Book Now</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 price-grid lost">
+					<div class="price-block agile">
+						<div class="price-gd-top">
+						<img src="images/r3.jpg" alt=" " class="img-responsive" />
+							<h4>Guest House</h4>
+						</div>
+						<div class="price-gd-bottom">
+							<div class="price-selet">
+								<h3><span>Rs</span>12,000</h3>
+								<a href="admin/reservation.php" >Book Now</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 price-grid wthree lost">
+					<div class="price-block agile">
+						<div class="price-gd-top ">
+							<img src="images/r4.jpg" alt=" " class="img-responsive" />
+							<h4>Single Room</h4>
+						</div>
+						<div class="price-gd-bottom">
+							<div class="price-selet">
+								<h3><span>Rs</span>10,000</h3>
+								<a href="admin/reservation.php" >Book Now</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+	 <!--// rooms & rates -->
+<!-- contact -->
+<section class="contact-w3ls" id="contact">
+
+		<div class="col-lg-6 col-md-6 col-sm-6 contact-w3-agile1" data-aos="flip-left">
+			<h4>Connect With Us</h4>
+			<p class="contact-agile1"><strong>Phone :</strong>+91-9798975645</p>
+			<p class="contact-agile1"><strong>Email :</strong> <a href="mailto:name@example.com">INFO@HOTELRIO.COM</a></p>
+			<p class="contact-agile1"><strong>Address :</strong> Bandra, Mumbai, India</p>
+</div>
+		<div class="clearfix"></div>
+	</div>
+</section>
+<!-- /contact -->
+			<div class="copy">
+		        <p>© 2019 HOTEL RIO . All Rights Reserved | Design by <a href="index.php">HOTEL RIO</a> </p>
+		    </div>
+<!--/footer -->
+<!-- js -->
+<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+<!-- contact form -->
+<script src="js/jqBootstrapValidation.js"></script>
+
+<!-- /contact form -->
+<!-- Calendar -->
+		<script src="js/jquery-ui.js"></script>
+		<script>
+				$(function() {
+				$( "#datepicker,#datepicker1,#datepicker2,#datepicker3" ).datepicker();
+				});
+		</script>
+<!-- //Calendar -->
+
+<!-- start-smoth-scrolling -->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+<!-- start-smoth-scrolling -->
+<script src="js/responsiveslides.min.js"></script>
+			<script>
+						// You can also use "$(window).load(function() {"
+						$(function () {
+						  // Slideshow 4
+						  $("#slider4").responsiveSlides({
+							auto: true,
+							pager:true,
+							nav:false,
+							speed: 500,
+							namespace: "callbacks",
+							before: function () {
+							  $('.events').append("<li>before event fired.</li>");
+							},
+							after: function () {
+							  $('.events').append("<li>after event fired.</li>");
+							}
+						  });
+
+						});
+			</script>
+
+<!--tabs-->
+<script src="js/easy-responsive-tabs.js"></script>
+<script>
+$(document).ready(function () {
+$('#horizontalTab').easyResponsiveTabs({
+type: 'default', //Types: default, vertical, accordion
+width: 'auto', //auto or any width like 600px
+fit: true,   // 100% fit in a container
+closed: 'accordion', // Start closed if in accordion view
+activate: function(event) { // Callback function if tab is switched
+var $tab = $(this);
+var $info = $('#tabInfo');
+var $name = $('span', $info);
+$name.text($tab.text());
+$info.show();
+}
+});
+$('#verticalTab').easyResponsiveTabs({
+type: 'vertical',
+width: 'auto',
+fit: true
+});
+});
+</script>
+<!--//tabs-->
+
+<script type="text/javascript" src="js/bootstrap-3.1.1.min.js"></script>
+</body>
+</html>
